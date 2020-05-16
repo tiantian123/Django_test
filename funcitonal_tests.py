e03280d645f39b1ae55c6b487b1890041827c025
chentian123 <2294300507@qq.com>
2020-05-16 16:48:45 +0800

    Functional test now checks we can input a to-do item

--- funcitonal_tests.py
+++ funcitonal_tests.py
@@ -17,9 +17,31 @@ class NewVisitorTest(unittest.TestCase):
 
         # 可以注意到网页的标题和头部都包含“To-Do” 这个词
         self.assertIn('To-Do', self.browser.title)
-        self.fail('finish the test')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # 应用邀请输入一个代办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        # 用户在一个文本框中输入了“buy peacock feathers(孔雀羽毛)”
+        # 爱好是使用假蝇做鱼铒钓鱼
+        inputbox.send_keys('Buy peacock feathers')
+
+        # 用户回车后页面更新
+        # 代办事项列表中显示了“1.Buy peacock feathers”
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+
+        # 页面又显示了一个文本框， 可以输入其他的代办事项
+        # 用户输入了 “Use peacock feathers to make a fly(使用孔雀羽毛做假蝇)”
+        # 用户做事很有条理
+        self.fail("Finish the test")
+
+        # 页面再次更新，用户的清单中显示了这2个待办事项
 
 if __name__ == '__main__':
     unittest.main()
