d6eaa0ff3f35cec07c56f750c9419873e1729586
chentian123 <2294300507@qq.com>
2020-05-16 11:33:16 +0800

    使用注释编写规格的首个功能测试，而且使用了unittest

--- funcitonal_tests.py
+++ funcitonal_tests.py
@@ -1,4 +1,25 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 听说有一个很酷的在线代办事项应用
+        # 现在去看一下这个应用的首页
+        self.browser.get('http://localhost:8000')
+
+        # 可以注意到网页的标题和头部都包含“To-Do” 这个词
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('finish the test')
+
+        # 应用邀请输入一个代办事项
+
+if __name__ == '__main__':
+    unittest.main()
