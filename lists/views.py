eb2be032c6be247568156dc67daf9c257bfcb694
chentian123 <2294300507@qq.com>
2020-05-18 01:08:04 +0800

    Redirect aters POST, and show all items in template

--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from lists.models import Item
+
 
 # Create your views here.
 @csrf_exempt
 def home_page(request):
-    # return HttpResponse('<html><title>To-Do lists</title></html？>')
-    # if request.method == 'POST':
-    #     return HttpResponse(request.POST['item_text'])
-    return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', ''),})
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
