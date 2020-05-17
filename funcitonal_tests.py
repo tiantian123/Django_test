4b6243ab5e23ef74367442e8ee0b5f4786e82220
chentian123 <2294300507@qq.com>
2020-05-17 16:19:59 +0800

    csrf 验证，以及添加表单内容测试

--- funcitonal_tests.py
+++ funcitonal_tests.py
@@ -32,18 +32,31 @@ class NewVisitorTest(unittest.TestCase):
         # 用户回车后页面更新
         # 代办事项列表中显示了“1.Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
-
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
-                        "New to-do item did not appear in table")
+        # self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
+        #                 "New to-do item did not appear in table -- its text was:\n%s" % (table.text,))
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # 页面又显示了一个文本框， 可以输入其他的代办事项
         # 用户输入了 “Use peacock feathers to make a fly(使用孔雀羽毛做假蝇)”
         # 用户做事很有条理
-        self.fail("Finish the test")
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
 
-        # 页面再次更新，用户的清单中显示了这2个待办事项
+        # 页面再次更新，清单中显示了2个代办事项
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+
+        # 用户想知道这个网站是否会记住她的清单
+        # 看到网站专门为她生成了一个唯一的URL
+        # 页面中有一些文字解说这个功能
+        self.fail("Finish the test")
+        # 用户访问那个URL, 发现代办事项清单还在
+
 
 if __name__ == '__main__':
     unittest.main()
--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-
+from django.views.decorators.csrf import csrf_exempt
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -11,8 +11,21 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+    @csrf_exempt
     def test_home_page_return_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+    @csrf_exempt
+    def test_home_page_can_save_a_post_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string('home.html', {
+            'new_item_text': 'A new list item'})
         self.assertEqual(response.content.decode(), expected_html)
